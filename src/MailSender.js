0cba37b4eedf46cb6c10cbe72539d6d220bafb66
muhamadRamadhaniZ <danii251018@gmail.com>
2023-06-13 10:14:05 +0700

    consumer program finished

---
+++ src/MailSender.js
@@ -0,0 +1,31 @@
+const nodeMailer = require('nodemailer');
+class MailSender {
+    constructor() {
+        this._transporter = nodeMailer.createTransport({
+            host: process.env.SMTP_HOST,
+            port: process.env.SMTP_PORT,
+            auth: {
+                user: process.env.SMTP_USER,
+                pass: process.env.SMTP_PASSWORD,
+            }
+        })
+    }
+
+    sendMail(targetEmail, content) {
+        const message = {
+            from: 'Notes Apps',
+            to: targetEmail,
+            subject: 'Ekspor Catatan',
+            text: 'Terlampir hasil dari ekspor catatan',
+            attachments: [
+              {
+                filename: 'notes.json',
+                content,
+              },
+            ],
+        };
+        return this._transporter.sendMail(message);
+    }
+}
+
+module.exports = MailSender;
